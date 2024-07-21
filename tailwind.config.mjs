2db4e6438820f6f2cb6af0e10046a9f3f933df14
tabbyistrans <nicholasrelyea110406@gmail.com>
2024-07-20 22:56:19 -0400

    Created Hero and Navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import catppuccin from "@catppuccin/daisyui";
+import daisyui from "daisyui";
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require(`daisyui`), require("@catppuccin/tailwindcss")],
+  daisyui: {
+    themes: [catppuccin("mocha", { primary: "teal" })],
+  },
+};
